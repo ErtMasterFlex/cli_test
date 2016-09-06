82b37761d24c3e499dac2269b6376e8e3331d462
Jason Dworken <jason.dworken@wirelesszone.com>
2016-09-06 14:36:33 -0400

    lets try this

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { 
+  Component,
+  trigger,
+  state,
+  style,
+  transition,
+  animate
+ } from '@angular/core';
+import { MdIconRegistry } from '@angular2-material/icon';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.css'],
+  animations: [
+    trigger('buttonState', [
+      state('inactive', style({
+        transform: 'scale(1)'
+      })),
+      state('active', style({
+        transform: 'scale(3.2)'
+      })),
+        transition('inactive => active', animate('100ms ease-in')),
+        transition('active => inactive',animate('100ms ease-out'))
+    ])
+
+  ]
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'app works';
+  progressShowing: boolean = true;
+  buttonState: string = 'inactive';
+
+  toggleState(){
+    if(this.buttonState == 'active'){
+      this.buttonState = 'inactive'
+    }
+    else{
+      this.buttonState = 'active'
+    }
+      
+  }
+  
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, ApplicationRef } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
-
 import { AppComponent } from './app.component';
+import { MdCoreModule, MdGestureConfig } from '@angular2-material/core';
+import { MdSidenavModule } from '@angular2-material/sidenav';
+import { MdInputModule } from '@angular2-material/input';
+import { MdButtonModule } from '@angular2-material/button';
+import { MdButtonToggleModule } from '@angular2-material/button-toggle';
+import { MdCardModule } from '@angular2-material/card';
+import { MdIconModule } from '@angular2-material/icon';
+import { MdListModule } from '@angular2-material/list';
+import { MdMenuModule } from '@angular2-material/menu';
+import { MdProgressBarModule } from '@angular2-material/progress-bar';
+import { MdProgressCircleModule } from '@angular2-material/progress-circle';
+import { MdRadioModule } from '@angular2-material/radio';
+import { MdSlideToggleModule } from '@angular2-material/slide-toggle';
+import { MdSliderModule } from '@angular2-material/slider';
+import { MdTabsModule } from '@angular2-material/tabs';
+import { MdToolbarModule } from '@angular2-material/toolbar';
+import { MdTooltipModule } from '@angular2-material/tooltip';
+import { MdRippleModule } from '@angular2-material/core/core';
 
 @NgModule({
   declarations: [
@@ -11,10 +28,31 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    CommonModule,
     FormsModule,
-    HttpModule
+    MdCoreModule,
+    MdSidenavModule,
+    MdInputModule,
+    MdButtonModule,
+    MdButtonToggleModule,
+    MdCardModule,
+    MdIconModule,
+    MdListModule,
+    MdMenuModule,
+    MdProgressBarModule,
+    MdProgressCircleModule,
+    MdRadioModule,
+    MdSliderModule,
+    MdSlideToggleModule,
+    MdTabsModule,
+    MdToolbarModule,
+    MdTooltipModule,
+    MdRippleModule
   ],
   providers: [],
+  entryComponents: [AppComponent],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+
+}
